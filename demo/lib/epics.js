b75a25d098203e0b72d2283deb16ab5789021a2b
Tomoya Nagasawa <ocrybit@gmail.com>
2020-05-31 02:37:56 +0200

    upgrade Next to ^9.4.4

--- demo/lib/epics.js
+++ demo/lib/epics.js
@@ -1,7 +1,7 @@
 import R from "ramdam"
 import { addEpic } from "nd-core"
-import * as nd from "../../.nextdapp"
-import * as custom from "./custom"
+import * as nd from ".nextdapp"
+import * as custom from "lib/custom"
 const predefined = R.mapObjIndexed((v, k, o) => addEpic(k, v))(nd)
 const custom_epics = R.mapObjIndexed((v, k, o) => addEpic(k, v))(custom)
 export default R.mergeLeft(custom_epics, predefined)
